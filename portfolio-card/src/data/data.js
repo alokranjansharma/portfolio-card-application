6f130b193ca80082598963a4d2382d2a6b8e27ef
alok <alok@infinitysoftsystems.com>
2026-01-17 11:14:26 +0530

    separated data file

---
+++ portfolio-card/src/data/data.js
@@ -0,0 +1,45 @@
+const cards = [
+    {
+        id: 1,
+        profilePic: "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_hybrid&w=740&q=80", 
+        name: "Alok",
+        title: "Frontend Engineer",
+        bio: "I design and build calm, focused product experiences for fast-moving teams. Currently exploring AI-assisted interfaces, design systems, and high-performance UI engineering.",
+        skills: ["Design System", "React", "TypeScript", "Figma", "Prototyping",
+            "Accessibility"
+        ],
+        likeCount: 268,
+        contact: "xyz22@gmail.com"
+    },
+    {
+        id: 2,
+        profilePic: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fHww", 
+        name: "Aarvi", 
+        title: "Junior Frontend Engineer",
+        bio: "I build responsive, user-friendly web interfaces using modern frontend technologies, focusing on performance, accessibility, and clean UI development.",
+        skills: ["Design System", "HTML", "CSS", "Javascript", "Prototyping",
+            "Accessibility"
+        ],
+        likeCount: 368,
+        contact: "abc@gmail.com"
+    },
+    {
+        id: 3,
+        profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzAsd5FvnXkMWeRpXu2Sq8e7ZluVQ95f7j4Q&s", 
+        name: "Vandana",
+        title: "SEO Engineer",
+        bio: "I optimize websites for search engines by improving on-page SEO, site performance, and technical SEO to increase visibility and organic traffic.",
+        skills: [
+            "On-Page SEO",
+            "Technical SEO",
+            "Keyword Research",
+            "Content Optimization",
+            "Meta Tags Optimization",
+            "Site Audit"
+        ],
+        likeCount: 158,
+        contact: "def@gmail.com"
+    },
+]
+
+export default cards;
